84f04e79863977edccb6c872ed495b13f2a00426
HuzaifaJunaid <Huzaifa.Junaid@arhamsoft.org>
2023-11-06 00:39:46 -0800

    Added

--- MyApp/android/app/src/main/java/com/myapp/MainActivity.java
+++ MyApp/android/app/src/main/java/com/myapp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.myapp;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
